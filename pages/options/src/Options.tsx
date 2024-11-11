33455334c67efb0e8a0f52362bce1b6a2be53ffc
Théophile Wallez <theophile.wallez@sia-partners.com>
2024-11-11 22:19:23 +0100

    Added emoji grid

--- pages/options/src/Options.tsx
+++ pages/options/src/Options.tsx
@@ -1,26 +1,21 @@
 import '@src/Options.css';
 import { useStorage, withErrorBoundary, withSuspense } from '@extension/shared';
 import { exampleThemeStorage } from '@extension/storage';
-import { Button } from '@extension/ui';
+import { allEmojis } from './data';
 
 const Options = () => {
   const theme = useStorage(exampleThemeStorage);
   const isLight = theme === 'light';
-  const logo = isLight ? 'options/logo_horizontal.svg' : 'options/logo_horizontal_dark.svg';
-  const goGithubSite = () =>
-    chrome.tabs.create({ url: 'https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite' });
 
   return (
     <div className={`App ${isLight ? 'bg-slate-50 text-gray-900' : 'bg-gray-800 text-gray-100'}`}>
-      <button onClick={goGithubSite}>
-        <img src={chrome.runtime.getURL(logo)} className="App-logo" alt="logo" />
-      </button>
-      <p>
-        Edit <code>pages/options/src/Options.tsx</code>
-      </p>
-      <Button className="mt-4" onClick={exampleThemeStorage.toggle} theme={theme}>
-        Toggle theme
-      </Button>
+      <div className="flex w-full flex-wrap gap-2">
+        {Object.entries(allEmojis).map(([key, emoji]) => (
+          <div key={key} className="flex size-10 items-center justify-center rounded-md bg-gray-200">
+            <img loading="lazy" src={emoji.src} alt={emoji.name} className="size-full" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
