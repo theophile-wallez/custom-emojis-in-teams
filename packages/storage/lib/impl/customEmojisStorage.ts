04f1a51b89c0f49c54b283c04245f0751acce44e
Théophile Wallez <theophile.wall@gmail.com>
2024-11-12 21:18:42 +0100

    feat: Init settings storage (#5)
    Co-authored-by: Théophile Wallez <theophile.wallez@sia-partners.com>

--- packages/storage/lib/impl/customEmojisStorage.ts
+++ packages/storage/lib/impl/customEmojisStorage.ts
@@ -4,21 +4,19 @@ import type { BaseStorage } from '../base/types';
 
 import { type CustomEmojisStorageMap } from '@extension/emojis';
 
-type Url = `http${'s' | ''}://${string}`;
-type CustomEmojiMap = Record<string, Url>;
 export type CustomEmojiStorage = BaseStorage<CustomEmojisStorageMap> & {
   reset: () => Promise<void>;
 };
 
-const defaultEmojis: CustomEmojiMap = {};
-const emojiStorage = createStorage<CustomEmojisStorageMap>('custom-emoji-storage', defaultEmojis, {
+const DEFAULT_STORAGE = {} as const satisfies CustomEmojisStorageMap;
+const newCustomEmojiStorage = createStorage<CustomEmojisStorageMap>('custom-emoji-storage', DEFAULT_STORAGE, {
   storageEnum: StorageEnum.Local,
   liveUpdate: true,
 });
 
 export const customEmojiStorage: CustomEmojiStorage = {
-  ...emojiStorage,
+  ...newCustomEmojiStorage,
   reset: async () => {
-    return await emojiStorage.set({});
+    return await newCustomEmojiStorage.set({});
   },
 };
--- packages/storage/lib/impl/index.ts
+++ packages/storage/lib/impl/index.ts
@@ -1,2 +1,3 @@
 export * from './exampleThemeStorage';
 export * from './customEmojisStorage';
+export * from './settingsStorage';
