8c1b58fa94e5128a1703ffe1ff62ebfb000e4c7a
Paul Planchon <paul.planchon@sia-partners.com>
2025-03-15 16:01:34 +0100

    test

--- pages/content/src/index.ts
+++ pages/content/src/index.ts
@@ -1,3 +1,4 @@
+import { cryptText } from './cryptText';
 import { findAndReplaceEmojis } from './findEmojis';
 import { getMappingStorage } from '@extension/storage';
 
@@ -14,6 +15,86 @@ async function initObserver(emojisMap: Record<string, string>) {
   });
 }
 
+let retry = 0;
+
+async function initMessageObserver() {
+  const messageElement = document.querySelector('[data-view^=message-pane-list-viewport]');
+
+  const messageObserver = new MutationObserver(cv => {
+    for (const mutation of cv) {
+      if (mutation.type === 'childList') {
+        cryptText(mutation.target);
+      }
+    }
+  });
+
+  if (!messageElement) {
+    if (retry > 3) return;
+
+    setTimeout(() => {
+      void initMessageObserver();
+      retry++;
+    }, 1000);
+    return;
+  }
+
+  messageObserver.observe(messageElement, {
+    childList: true,
+    subtree: true
+  });
+}
+
+let retryReplaceInputBar = 0;
+
+function replaceInputBar() {
+  console.log('replaceInputBar');
+  const inputBar = document.querySelector('[contenteditable="true"]') as HTMLElement;
+  // cerate a new div with the same style as the inputBar
+  // above the inputBar
+  if (!inputBar) {
+    if (retryReplaceInputBar > 10) {
+      console.error('Failed to replace input bar');
+      return;
+    }
+
+    setTimeout(() => {
+      void replaceInputBar();
+      retryReplaceInputBar++;
+    }, 1500);
+    return;
+  }
+
+  console.log('inputBar', inputBar);
+
+  // create a new div with the same style as the inputBar
+  // above the inputBar
+  const myInput = document.createElement('div');
+  myInput.id = 'myInput';
+  myInput.style.position = 'absolute';
+  myInput.style.left = `400px`;
+  myInput.style.bottom = `90px`;
+  myInput.style.width = `500px`;
+  myInput.style.height = `40px`;
+  myInput.style.zIndex = '9999999';
+  myInput.style.backgroundColor = 'red';
+  myInput.contentEditable = 'true';
+  document.body.appendChild(myInput);
+
+  myInput.addEventListener('keydown', e => {
+    if (e.key === 'Enter') {
+      console.log('Enter');
+      const message = myInput.innerText;
+      console.log('message', message);
+      inputBar.dispatchEvent(new KeyboardEvent('keydown', { bubbles: true, cancelable: true, key: 'a' }));
+    }
+  });
+}
+
 getMappingStorage().then(response => {
   void initObserver(response);
+  setTimeout(() => {
+    void initMessageObserver();
+  }, 5000);
 });
+
+replaceInputBar();
